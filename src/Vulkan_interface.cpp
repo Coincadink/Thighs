56fb7a1648c935b9638a05821800f1c83af0ed60
John Peterson <johnp5419@gmail.com>
2022-06-02 21:00:19 -0600

    Editing build settings

--- src/Vulkan_interface.cpp
+++
@@ -1,1 +0,0 @@
-#include "Vulkan_interface.hpp"
--- src/Vulkan_interface.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef VULKAN_INTERFACE_HPP_
-#define VULKAN_INTERFACE_HPP_
-
-#define VULKAN_HPP_NO_CONSTRUCTORS
-#include <vulkan/vulkan.hpp>
-#include <GLFW/glfw3.h>
-
-#endif // VULKAN_INTEFACE_HPP_
