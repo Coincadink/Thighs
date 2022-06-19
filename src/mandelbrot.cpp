6d12a99dc72bc46381c0e559dfb23d0762e45de8
John Peterson <johnp5419@gmail.com>
2022-06-18 19:20:45 -0600

    PNG Output!

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -1,25 +1,32 @@
 #include "Vulkan_mandelbrot_generator.hpp"
+#include "lodepng.h"
 #include <iostream>
+
+void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
+  //Encode the image
+  unsigned error = lodepng::encode(filename, image, width, height);
+
+  //if there's an error, display it
+  if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+}
 
 int main() 
 {
-  int width = 4800;
-  int height = 3200;
+  const char* filename = "mandelbrot.png";
 
-  // Max size for intel integrated is 32.
+  int width = 9600;
+  int height = 6400;
+  // Max size for intel integrated 520 is 32.
   uint32_t workgroup_size = 32;
 
   auto raw_image = std::vector<unsigned char>{};
   raw_image = Vulkan_mandelbrot_generator{width, height, workgroup_size}.generate();
 
-  int pixelTest = -1;
+//   std::cout << "P3\n" << width << " " << height << "\n255\n";
+//   for (int i = 0; i < raw_image.size(); i++) 
+//   {
+//       std::cout << static_cast<unsigned int>(raw_image[i]);
+//   }
 
-  std::cout << "P3\n" << width << " " << height << "\n255\n";
-  for (int i = 0; i < raw_image.size(); i++) 
-  {
-      int index = i % 3;
-      if (index == 0) pixelTest ++;
-
-      if (!(pixelTest % 4 == 0)) std::cout << static_cast<unsigned int>(raw_image[i]) << " ";
-  }
+  encodeOneStep(filename, raw_image, width, height);
 }
